0305c44e0d00ed69da31e038614e220996fd1557
Karen Garcia <karen.garcia@nexscreen.com>
2019-07-31 14:47:12 -0600

    initial commit for my portfolio, used my previous site I made and adjusting to my needs for portfolio

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavigationBar from './Navigation';
+import Footer from './Footer';
+// import Home from './HomeSection';
+
+const App = () => (
+  <div>
+    <NavigationBar />
+    <Footer />
+  </div>
+) 
+
+export default App;
