8200015dc177bb9b835e173cfb4e0a94da1eb788
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-03 15:39:31 -0600

    figured out object access to move my sayKaren logo state to different sections of the object

--- src/Components/App/LogoState.js
+++ src/Components/App/LogoState.js
@@ -6,7 +6,8 @@ const LogoState = () =>{
     0: "...Build me a website",
     1: "...Teach me to code",
     2: "...Build a Vanilla JavaScript website",
-    3: "...Build a React website",
+    3: "...Market my business",
+    4: "...Build a React website"
   };
   
   const [greeting, setGreeting] = useState(
@@ -16,18 +17,31 @@ const LogoState = () =>{
 
 
   const handleChange = () => {
+    //Filter function to an Object
     Object.filter = (obj, predicate) =>
       Object.keys(obj)
             .filter(key=> predicate(obj[key]) )
             .reduce( (res, key) => (res[key] = obj[key], res), {}
     );
+
+    // Determine last variable item. 
     var comparing = Object.filter(workStates, x => x === "...Build a React website");
-    var comparingNow = Object.filter(workStates, x => x === currentValue);
+    //Variable to determine the last item to know when to change it back to 0 in the object workStates
     var lastItem = Object.values(comparing).toString();
 
     //greeting and CurrentValue is a string originally "...Build a React website" string to be concise
-    var currentValue = greeting;
-    
+    var currentValue = Object.filter(workStates, x => x === greeting); //  {0: "...Build me a website"} object
+    var currentKey = Object.keys(currentValue); // pulls the key of that currentValue
+    // console.log({currentValue});
+    // console.log("value of current" +currentKey[0]);
+
+
+    var workingOn = currentKey[0]; // string
+    var currentKeyValue = parseInt(workingOn);
+    var finalResultAdded = currentKeyValue +1; // working to move it to another number 
+    console.log(currentKey[0]); //right now 0 or 3 as that is what I am changing
+    console.log("type of currentKeyValue I need to add to" +typeof(currentKeyValue));
+    console.log({finalResultAdded});
     // var correctOrNot = Object.filter(workStates, x => (Object.values(x) === greeting));
     // console.log({correctOrNot});
     // console.log(Object.values(correctOrNot));
@@ -39,13 +53,18 @@ const LogoState = () =>{
     // console.log("current value type " +typeof(currentValue)); //current value type string
     // console.log("does current value and greeting match" + (currentValue === greeting));
     // var lastItemString = lastItem.toString();
+  
     console.log(Object.keys(comparing)); // 3
     console.log(Object.values(comparing));// ["...Build a React website"]
+    
 
     console.log(` at end should be true ${greeting === lastItem}`);
 
     if (greeting !== lastItem){
-      setGreeting(workStates[3]);
+      var workingOn = currentKey[0]; // string
+      var currentKeyValue = parseInt(workingOn);
+      var finalResultAdded = currentKeyValue +1;
+      setGreeting(workStates[finalResultAdded]);
     }else{
       setGreeting(workStates[0])
     };
