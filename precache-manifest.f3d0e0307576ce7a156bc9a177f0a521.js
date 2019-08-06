38587a95a257f1e923ed4b975d4b8354490bc247
Karen Garcia <karen.garcia@nexscreen.com>
2019-08-05 21:09:50 -0600

    Initial dist subtree commit

---
+++ precache-manifest.f3d0e0307576ce7a156bc9a177f0a521.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182cdf2c200420e60085482b8e1e42cc",
+    "url": "/sayKaren/index.html"
+  },
+  {
+    "revision": "0150d99b52b66cabce5c",
+    "url": "/sayKaren/static/css/main.be1473cd.chunk.css"
+  },
+  {
+    "revision": "7ebb4b2cf6dd0c8ff4eb",
+    "url": "/sayKaren/static/js/2.30976c69.chunk.js"
+  },
+  {
+    "revision": "0150d99b52b66cabce5c",
+    "url": "/sayKaren/static/js/main.13f3c638.chunk.js"
+  },
+  {
+    "revision": "093d96e9b116f39c03ae",
+    "url": "/sayKaren/static/js/runtime~main.1b97a926.js"
+  },
+  {
+    "revision": "7a737d02eb03b76e030334a473132d90",
+    "url": "/sayKaren/static/media/Bomdigity.7a737d02.gif"
+  },
+  {
+    "revision": "2099b4832a54e318deaaa2908f793138",
+    "url": "/sayKaren/static/media/Karen_freeCodeCamp_Certificate_Responsive_Web_Design_2.13.2019.2099b483.png"
+  },
+  {
+    "revision": "7f28071e5bee2a6f88689506534d2d30",
+    "url": "/sayKaren/static/media/Number_Guesser.7f28071e.gif"
+  },
+  {
+    "revision": "56952159168e1c475dc1edeb1a7be781",
+    "url": "/sayKaren/static/media/React_Website_ES6_8.1.2019.56952159.png"
+  },
+  {
+    "revision": "230b6c8b8f16c40b30484e727a311da5",
+    "url": "/sayKaren/static/media/checkwritting.230b6c8b.png"
+  },
+  {
+    "revision": "bd82fb9a7ee7894d890bdf0d6bedc8bf",
+    "url": "/sayKaren/static/media/flashcard-starter.bd82fb9a.gif"
+  },
+  {
+    "revision": "f00add6dbcf78651995998cfebba726f",
+    "url": "/sayKaren/static/media/karenHero.f00add6d.jpg"
+  },
+  {
+    "revision": "4ee98378ff2cd65d37dc47e7b6cd0023",
+    "url": "/sayKaren/static/media/sayKaren_Logo_transparent_7.2019.4ee98378.png"
+  },
+  {
+    "revision": "d0827d01ac17b128172a9f867b8d0c82",
+    "url": "/sayKaren/static/media/sayKaren_logo-white_8.1.2019.d0827d01.png"
+  }
+]);
